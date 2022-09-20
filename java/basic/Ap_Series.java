a441adc4dba58f99b5c58ef53469e989066b6c53
kuldeep panwar <kuldeeppanwar7697@gmail.com>
2022-09-20 19:05:03 +0530

    ok

---
+++ java/basic/Ap_Series.java
@@ -0,0 +1,19 @@
+public class Ap_Series {
+    public static void main(String[] args) {
+        int term = 10;
+        int diff =  4;
+        int sum = 0;
+        int j =1;
+        for(int i =1 ; i<=term; i++){
+            sum=sum+j;
+            System.out.println(j);
+            j=j+diff;
+        }
+        System.out.println("sum of ap series :"+sum);
+    
+    }
+    
+}
+
+    
+
